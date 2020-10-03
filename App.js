80b9eb1eecf7ec2b84073dee138184b67967fb49
Momen Fnnana <moamenfnnana1@gmail.com>
2020-10-03 10:39:34 +0300

    ...

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Started New project</Text>
+      <Text>Started Taxi project</Text>
       <StatusBar style="auto" />
     </View>
   );

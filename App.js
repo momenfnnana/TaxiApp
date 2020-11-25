a3cdb862e0151f3ec31c902d2915c92d2bcb7279
Momen Fnnana <moamenfnnana1@gmail.com>
2020-11-25 04:12:21 +0200

    adding google api key for android and customizing map

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE,Marker } from "react-native-maps";
+import MapStyle from "./MapStyle.js"
+import * as Location from 'expo-location';
+
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+const LATITUDE = 18.7934829;
+const LONGITUDE = 98.9867401;
 
 export default function App() {
+  const [latitude, setLatitude] = useState(LATITUDE);
+  const [longitude, setLongitude] = useState(LONGITUDE);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log("location", location);
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  const getMapRegion = () => ({
+    latitude: latitude,
+    longitude: longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  });
   return (
     <View style={styles.container}>
-      <Text>Started Taxi project</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{ ...StyleSheet.absoluteFillObject }}
+        region={getMapRegion()}
+        customMapStyle={MapStyle}
+      >
+      <Marker
+        coordinate={getMapRegion()}
+      />
+    </MapView>
       <StatusBar style="auto" />
     </View>
   );
